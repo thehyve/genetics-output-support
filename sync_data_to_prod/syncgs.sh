48b0df4d1218fabbfcec2cee820376bb4007dc7a
Cinzia <cinzia.malangone@gmail.com>
2021-11-25 08:42:18 +0000

    fix syncgs

--- sync_data_to_prod/syncgs.sh
+++ sync_data_to_prod/syncgs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-pre_data_release="gs://${GS_SYNC_FROM}/"
+pre_data_release="gs://${GS_SYNC_FROM}/*"
+echo $pre_data_release
 
 echo "=== Start copying..."
-cmd=`gsutil -m cp -r $pre_data_release gs://open-targets-genetics-releases /${RELEASE_ID_PROD}".02"/`
+cmd=`gsutil -m cp -r $pre_data_release gs://open-targets-genetics-releases/${RELEASE_ID_PROD}".02"/`
 echo "=== Copying done."
