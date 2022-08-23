c282cb50b8dcbc8ab98c92b3fccf5a5f19cd29f9
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-08-23 11:52:15 +0100

    Add deployment branch to CH module

--- terraform_create_images/modules/clickhouse/main.tf
+++ terraform_create_images/modules/clickhouse/main.tf
@@ -1,7 +1,7 @@
 resource "random_string" "random" {
-  length = 8
-  lower = true
-  upper = false
+  length  = 8
+  lower   = true
+  upper   = false
   special = false
   keepers = {
     // Take into account changes in the machine type
@@ -24,17 +24,17 @@ resource "google_compute_instance" "clickhouse_etl" {
   // Good, we need randomness in case we make changes in the VM that will replace it
   name = "${var.module_wide_prefix_scope}-server-${random_string.random.result}"
   // We are launching only one VM, so we can externalise the machine type computation
-  machine_type = local.vm_machine_type
-  zone   = var.vm_default_zone
+  machine_type              = local.vm_machine_type
+  zone                      = var.vm_default_zone
   allow_stopping_for_update = true
-  can_ip_forward = true
-  count = var.enable_module
+  can_ip_forward            = true
+  count                     = var.enable_module
 
   boot_disk {
     initialize_params {
-      image =  var.vm_clickhouse_boot_image
-      type = "pd-ssd"
-      size = var.vm_clickhouse_boot_disk_size
+      image = var.vm_clickhouse_boot_image
+      type  = "pd-ssd"
+      size  = var.vm_clickhouse_boot_disk_size
     }
   }
 
@@ -47,20 +47,21 @@ resource "google_compute_instance" "clickhouse_etl" {
 
   metadata = {
     startup-script = templatefile(
-   "${path.module}/scripts/clickhouse_startup.sh",
+      "${path.module}/scripts/clickhouse_startup.sh",
       {
-      PROJECT_ID = var.project_id,
-      GC_ZONE = var.vm_default_zone,
-      GS_ETL_DATASET = var.gs_etl
-    }
+        PROJECT_ID     = var.project_id,
+        GC_ZONE        = var.vm_default_zone,
+        GS_ETL_DATASET = var.gs_etl,
+        DEP_BRANCH     = var.dep_branch
+      }
     )
     google-logging-enabled = true
   }
 
 
   service_account {
-    email = "pos-service-account@${var.project_id}.iam.gserviceaccount.com"
-    scopes = [ "cloud-platform" ]
+    email  = "pos-service-account@${var.project_id}.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
   }
 
   // Upon changes to the VM, it will create the new one before getting rid of the previous one
--- terraform_create_images/modules/clickhouse/variables.tf
+++ terraform_create_images/modules/clickhouse/variables.tf
@@ -2,54 +2,60 @@
 // General deployment input parameters --- //
 variable "enable_module" {
   description = "Enable/disable the module CH"
-  type = number
-  default = 1
+  type        = number
+  default     = 1
 }
 
 variable "module_wide_prefix_scope" {
   description = "The prefix provided here will scope names for those resources created by this module, default 'otpdevch'"
-  type = string
-  default = "otpdevch"
+  type        = string
+  default     = "otpdevch"
 }
 
 variable "project_id" {
   description = "Default project to use when not specified in the module"
-  type = string
+  type        = string
 }
 
 variable "vm_clickhouse_boot_image" {
   description = "Boot image configuration for the deployed Clickhouse Instances"
-  type = string
-  default = "debian-10"
+  type        = string
+  default     = "debian-10"
 }
 
 variable "vm_default_region" {
   description = "Default region when not specified in the module"
-  type = string
+  type        = string
 }
 
 variable "vm_default_zone" {
   description = "Default zone when not specified in the module"
-  type = string
+  type        = string
 }
 
 variable "vm_clickhouse_boot_disk_size" {
   description = "Clickhouse instances boot disk size, default '500GB'"
-  type = string
-  default = 300
+  type        = string
+  default     = 300
 }
 
 variable "vm_clickhouse_vcpus" {
   description = "CPU count for each Clickhouse Node VM"
-  type = number
+  type        = number
 }
 
 variable "vm_clickhouse_mem" {
   description = "Amount of memory assigned to every Clickhouse Instance (MiB)"
-  type = number
+  type        = number
 }
 
 variable "gs_etl" {
   description = "Output of the ETL [root]. Eg. open-targets-data-releases/21.04/output"
-  type = string
+  type        = string
+}
+
+variable "dep_branch" {
+  description = "The git branch to gather the SQL loading scripts from."
+  type        = string
+  default     = "main"
 }
