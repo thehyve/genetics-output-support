92d082da83b8dca484d8b798dd28c0fe491d00ac
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-08-30 21:48:28 +0100

    Rename posvm module to loader_vm

--- terraform_create_images/modules/posvm/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  roles   = ["roles/compute.admin", "roles/logging.viewer","roles/compute.instanceAdmin","roles/storage.objectViewer","roles/storage.admin"]
-  remote_user_name = "provisioner"
-}
--- terraform_create_images/modules/posvm/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "pos_support_vm_name" {
-  value = join("",google_compute_instance.pos_vm.*.name)
-}
