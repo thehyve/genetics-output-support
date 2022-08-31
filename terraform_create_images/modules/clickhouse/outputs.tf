c55d705a6bc16d770e07f37d328f2a389e51c8e2
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-08-31 09:58:16 +0100

    Delete unusd modules

--- terraform_create_images/modules/clickhouse/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "clickhouse_vm_name" {
-  value =join("", google_compute_instance.clickhouse_etl.*.name)
-}
--- terraform_create_images/modules/elasticsearch/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "elasticsearch_vm_name" {
-  value =join("", google_compute_instance.elasticsearch_etl.*.name)
-}
