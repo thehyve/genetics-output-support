3c1fdc2c873fd682f168382015a01700929a6faa
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-10-11 19:49:21 +0100

    parallelize rsync operation between buckets

--- sync_data_to_prod/syncgs.sh
+++ sync_data_to_prod/syncgs.sh
@@ -4,5 +4,5 @@ pre_data_release="gs://${GS_SYNC_FROM}/"
 echo $pre_data_release
 
 echo "=== Start copying..."
-cmd=`gsutil rsync $pre_data_release gs://open-targets-genetics-releases/${RELEASE_ID_PROD}".01"/`
+cmd=`gsutil -m rsync -r $pre_data_release gs://open-targets-genetics-releases/${RELEASE_ID_PROD}".01"/`
 echo "=== Copying done."
